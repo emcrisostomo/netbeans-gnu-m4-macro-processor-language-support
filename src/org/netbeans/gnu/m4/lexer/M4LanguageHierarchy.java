b6cbac4fdd7205b1313b12ab4a82a3d0cb888983
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2014-03-21 12:49:08 +0100

    Add M4 token id class.

---
+++ src/org/netbeans/gnu/m4/lexer/M4LanguageHierarchy.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.netbeans.gnu.m4.lexer;
+
+/**
+ *
+ * @author Enrico M. Crisostomo
+ */
+public class M4LanguageHierarchy {
+    
+}
---
+++ src/org/netbeans/gnu/m4/lexer/M4TokenId.java
@@ -0,0 +1,56 @@
+/* 
+ * Copyright (C) 2014, Enrico M. Crisostomo
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.netbeans.gnu.m4.lexer;
+
+import org.netbeans.api.lexer.Language;
+import org.netbeans.api.lexer.TokenId;
+
+/**
+ *
+ * @author Enrico M. Crisostomo
+ */
+public class M4TokenId implements TokenId {
+
+    private final String name;
+    private final String primaryCategory;
+    private final int id;
+
+    public M4TokenId(String name, String primaryCategory, int id) {
+        this.name = name;
+        this.primaryCategory = primaryCategory;
+        this.id = id;
+    }
+
+    @Override
+    public String name() {
+        return name;
+    }
+
+    @Override
+    public int ordinal() {
+        return id;
+    }
+
+    @Override
+    public String primaryCategory() {
+        return primaryCategory;
+    }
+
+    public static Language<M4TokenId> getLanguage() {
+        return new M4LanguageHierarchy().language();
+    }
+}
