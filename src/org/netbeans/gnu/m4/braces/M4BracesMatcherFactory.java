be4c0ddbdf677a305392a9fc232cb4b309068ba3
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2014-03-21 13:47:42 +0100

    Use default brace matcher factory for m4 files.

---
+++ src/org/netbeans/gnu/m4/braces/M4BracesMatcherFactory.java
@@ -0,0 +1,36 @@
+/* 
+ * Copyright (C) 2014, Enrico M. Crisostomo
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.netbeans.gnu.m4.braces;
+
+import org.netbeans.api.editor.mimelookup.MimeRegistration;
+import org.netbeans.spi.editor.bracesmatching.BracesMatcher;
+import org.netbeans.spi.editor.bracesmatching.BracesMatcherFactory;
+import org.netbeans.spi.editor.bracesmatching.MatcherContext;
+import org.netbeans.spi.editor.bracesmatching.support.BracesMatcherSupport;
+
+/**
+ *
+ * @author Enrico M. Crisostomo
+ */
+@MimeRegistration(mimeType = "text/x-m4", service = BracesMatcherFactory.class)
+public class M4BracesMatcherFactory implements BracesMatcherFactory {
+
+    @Override
+    public BracesMatcher createMatcher(MatcherContext context) {
+        return BracesMatcherSupport.defaultMatcher(context, -1, -1);
+    }
+}
