18dcec1e8cc84c4aa2c47761d9ee1a0cab393147
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2014-04-02 19:12:47 +0200

    Use the identifier primary category for a generic identifier.

--- netbeans-gnu-m4-macro-processor-language-support/src/org/netbeans/gnu/m4/lexer/antlr/M4LanguageHierarchy.java
+++ netbeans-gnu-m4-macro-processor-language-support/src/org/netbeans/gnu/m4/lexer/antlr/M4LanguageHierarchy.java
@@ -38,7 +38,7 @@ public final class M4LanguageHierarchy extends LanguageHierarchy<M4TokenId> {
         int regExCounter = 1;
         tokens = Arrays.asList(new M4TokenId[]{
             new M4TokenId("DNL_COMMENT", "comment", regExCounter++),
-            new M4TokenId("ID", "keyword", regExCounter++),
+            new M4TokenId("ID", "identifier", regExCounter++),
             new M4TokenId("LPAREN", "separator", regExCounter++),
             new M4TokenId("RPAREN", "separator", regExCounter++),
             new M4TokenId("LBRACKET", "separator", regExCounter++),
