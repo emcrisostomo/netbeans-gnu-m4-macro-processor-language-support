2ae10cbc0cf298380512c91601bc33a0d647e2f3
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2014-04-03 18:42:41 +0200

    Update the semantic highlighter to use the results of the ANTLR parser.

--- netbeans-gnu-m4-macro-processor-language-support/src/org/netbeans/gnu/m4/semantic/M4SemanticHighlighter.java
+++ netbeans-gnu-m4-macro-processor-language-support/src/org/netbeans/gnu/m4/semantic/M4SemanticHighlighter.java
@@ -23,19 +23,15 @@ import java.util.IdentityHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.SwingUtilities;
-import javax.swing.text.AbstractDocument;
 import javax.swing.text.Document;
+import javax.swing.text.StyledDocument;
 import org.netbeans.api.lexer.Token;
-import org.netbeans.api.lexer.TokenHierarchy;
 import org.netbeans.api.lexer.TokenId;
-import org.netbeans.api.lexer.TokenSequence;
 import static org.netbeans.gnu.m4.Constants.isVerbose;
-import org.netbeans.gnu.m4.antlr.v0.m4Parser;
+import org.netbeans.gnu.m4.antlr.support.ANTLRTokenToNetBeansTokenMapper;
 import org.netbeans.gnu.m4.antlr.v1.m4Parser.CompilationUnitContext;
-import org.netbeans.gnu.m4.lexer.antlr.M4TokenId;
 import org.netbeans.gnu.m4.parser.M4Parser;
 import org.netbeans.gnu.m4.semantic.M4ColoringAttributes.Coloring;
 import org.netbeans.modules.parsing.spi.IndexingAwareParserResultTask;
@@ -51,131 +47,125 @@ import org.netbeans.spi.editor.hints.ErrorDescription;
  * @author Enrico M. Crisostomo
  */
 class M4SemanticHighlighter extends IndexingAwareParserResultTask<Result> {
-    
+
     private static final Logger logger = Logger.getLogger(M4SemanticHighlighter.class.getName());
-    
+
     M4SemanticHighlighter() {
         super(TaskIndexingMode.ALLOWED_DURING_SCAN);
     }
-    
+
     @Override
     public void run(Result result, SchedulerEvent event) {
         if (isVerbose()) {
             logger.info("Running.");
         }
-        
+
         if (result == null) {
             logger.severe("Parser result is null.");
             return;
         }
-        
+
         if (!(result instanceof M4Parser.M4ParserResult)) {
             logger.severe(String.format("Invalid result class: %s", result.getClass().getName()));
             return;
         }
-        
+
+        final Document doc = result.getSnapshot().getSource().getDocument(false);
+
+        if (!(doc instanceof StyledDocument)) {
+            throw new IllegalStateException(
+                    String.format(
+                            "Unexpected document class [%s] is not instance of [%s].",
+                            doc.getClass().getName(),
+                            StyledDocument.class.getName()));
+        }
+
         M4Parser.M4ParserResult res = (M4Parser.M4ParserResult) result;
         CompilationUnitContext compilationUnit = res.getCompilationUnit();
+        M4BuiltinMacroVisitor visitor = new M4BuiltinMacroVisitor();
+        visitor.visit(compilationUnit);
+
+        final Set<org.antlr.v4.runtime.Token> builtinIdentifiers = visitor.getBuiltinIdentifiers();
+
+        final List<M4ColoringAttributes> builtinSpec = new ArrayList<>();
+        builtinSpec.add(M4ColoringAttributes.M4_BUILTIN);
+        final Coloring builtinColoring = collection2Coloring(builtinSpec);
+        final ANTLRTokenToNetBeansTokenMapper mapper = new ANTLRTokenToNetBeansTokenMapper(doc);
         
-        
-        final Document doc = result.getSnapshot().getSource().getDocument(false);
-        
-        process(doc);
+        final Map<Token<? extends TokenId>, Coloring> newColoring = new IdentityHashMap<>();
+        final Set<Token<? extends TokenId>> addedTokens = new HashSet<>();
+        final Map<Token<? extends TokenId>, Coloring> removedTokens = new IdentityHashMap<>(M4LexerBasedHighlightLayer.getLayer(M4SemanticHighlighter.class, doc).getColorings());
+
+        for (org.antlr.v4.runtime.Token antlrToken : builtinIdentifiers) {
+            logger.info(String.format("M4 Builtin Token Position: %d %d %d.", antlrToken.getLine(), antlrToken.getCharPositionInLine(), antlrToken.getText().length()));
+
+            Token<? extends TokenId> token = mapper.findToken(antlrToken);
+
+            if (token == null) {
+                continue;
+            }
+
+            newColoring.put(token, builtinColoring);
+            
+            Coloring oldColoring = removedTokens.remove(token);
+            
+            if (oldColoring == null || !oldColoring.equals(builtinColoring)) {
+                addedTokens.add(token);
+            }
+        }
+
+        ERROR_DESCRIPTION_SETTER.setColorings(doc, newColoring, addedTokens);
     }
-    
+
     static Coloring collection2Coloring(Collection<M4ColoringAttributes> attr) {
         Coloring c = M4ColoringAttributes.empty();
-        
+
         for (M4ColoringAttributes a : attr) {
             c = M4ColoringAttributes.add(c, a);
         }
-        
+
         return c;
     }
-    
-    private void process(final Document document) {
-        Map<Token<? extends TokenId>, Coloring> newColoring = new IdentityHashMap<>();
-        Map<Token<? extends TokenId>, Coloring> oldColors = M4LexerBasedHighlightLayer.getLayer(M4SemanticHighlighter.class, document).getColorings();
-        Map<Token<? extends TokenId>, Coloring> removedTokens = new IdentityHashMap<>(oldColors);
-        Set<Token<? extends TokenId>> addedTokens = new HashSet<>();
-        
-        AbstractDocument doc = (AbstractDocument) document;
-        
-        doc.readLock();
-        
-        try {
-            TokenHierarchy<AbstractDocument> ti = TokenHierarchy.get(doc);
-            TokenSequence<? extends TokenId> ts = ti.tokenSequence();
-            
-            while (ts.moveNext()) {
-                Token<? extends TokenId> t = ts.token();
 
-                // Ignore tokens which are not our instances
-                // (used only as a cast safeguard).
-                if (!(t.id() instanceof M4TokenId)) {
-                    continue;
-                }
-                
-                List<M4ColoringAttributes> spec = new ArrayList<>();
-                spec.add(M4ColoringAttributes.M4_MACRO);
-                spec.add(M4ColoringAttributes.DECLARATION);
-                
-                Coloring c = collection2Coloring(spec);
-                
-                newColoring.put(t, c);
-                
-                Coloring oldColoring = removedTokens.remove(t);
-                
-                if (oldColoring == null || !oldColoring.equals(c)) {
-                    addedTokens.add(t);
-                }
-            }
-        } finally {
-            doc.readUnlock();
-        }
-        
-        ERROR_DESCRIPTION_SETTER.setColorings(doc, newColoring, addedTokens);
-    }
-    
     @Override
     public int getPriority() {
         return 100;
     }
-    
+
     @Override
     public Class<? extends Scheduler> getSchedulerClass() {
         return Scheduler.EDITOR_SENSITIVE_TASK_SCHEDULER;
     }
-    
+
     @Override
     public void cancel() {
         // no op
     }
-    
+
     public static interface ErrorDescriptionSetter {
-        
+
         public void setErrors(Document doc, List<ErrorDescription> errors);
-        
+
         public void setHighlights(Document doc, OffsetsBag highlights);
-        
+
         public void setColorings(Document doc, Map<Token<? extends TokenId>, Coloring> colorings, Set<Token<? extends TokenId>> addedTokens);
     }
-    
+
     private static final ErrorDescriptionSetter ERROR_DESCRIPTION_SETTER = new ErrorDescriptionSetter() {
         @Override
         public void setErrors(Document doc, List<ErrorDescription> errors) {
         }
-        
+
         @Override
         public void setHighlights(final Document doc, final OffsetsBag highlights) {
             SwingUtilities.invokeLater(new Runnable() {
                 @Override
                 public void run() {
-                    
+
                 }
             });
         }
-        
+
         @Override
         public void setColorings(final Document doc, final Map<Token<? extends TokenId>, Coloring> colorings, final Set<Token<? extends TokenId>> addedTokens) {
             SwingUtilities.invokeLater(new Runnable() {
