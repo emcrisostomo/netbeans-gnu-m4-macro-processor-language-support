8b16656c61e8130c10420400711810a7dd3986ea
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2014-04-04 14:08:58 +0200

    Remove unused Swing imports.

--- netbeans-gnu-m4-macro-processor-language-support/src/org/netbeans/gnu/m4/semantic/M4SemanticHighlighter.java
+++ netbeans-gnu-m4-macro-processor-language-support/src/org/netbeans/gnu/m4/semantic/M4SemanticHighlighter.java
@@ -24,7 +24,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.logging.Logger;
-import javax.swing.SwingUtilities;
 import javax.swing.text.Document;
 import org.netbeans.api.lexer.Token;
 import org.netbeans.api.lexer.TokenId;
